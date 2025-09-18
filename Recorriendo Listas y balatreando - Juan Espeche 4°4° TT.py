9c305bce9fd9d8cd970d55a2d131ea8758780667
juanespeche_et32 <juan.especheet32@gmail.com>
2025-09-18 13:16:52 -0300

    Add files via upload

--- Recorriendo Listas y balatreando - Juan Espeche 4°4° TT.py
+++ Recorriendo Listas y balatreando - Juan Espeche 4°4° TT.py
@@ -1,3 +1,4 @@
+import random
 def ejercicio1():
     nombres = ["Juan", "Demian", "Travieso", "Bruno", "Agustin", "Alba", "Thiago", "Santiago", "Josué", "Leonel"]
     nombre_largo = ""
@@ -25,17 +26,53 @@ def ejercicio3():
     print(f"La lista queda: {lista_factor}")
 def ejercicio4():
     print("¡Bienvenido al Balatro!")
-    num_cartas = []
-    palos = []
+    num_cartas = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
+    palos = [["Espadas"], ["Treboles"], ["Corazones"], ["Diamantes"]]
+    mazo = []
     seguir = True
-    while seguir == True:
+    while seguir:
         menu = int(input(
-            """
+            f"""
 ----------------------
 1.Pedir cartas
 2.Descartar cartas
 3.Dejar de jugar
 ----------------------
+Tus cartas son: {mazo}
 Seleccione una opcion: """))
         if menu == 1:
-
+            cartas = 8
+            while cartas != 0:
+                num_cartas = num_cartas[random.randint(0, 9)][random.randint(0, 9)]
+                palos = palos[random.randint(0, 3)][random.randint(0, 3)]
+                mazo.append(num_cartas)
+                mazo.append(palos)
+        if menu == 2:
+            mazo.remove(num_cartas)
+            mazo.remove(palos)
+        if menu == 3:
+            seguir = False
+def menu():
+    seguir = True
+    while seguir:
+        opc = int(input("""
+        ---------------------------
+        1.Nombres
+        2.Vocales en nombres
+        3.Factor y lista de numeros
+        4.BALATRO
+        0.Salir del programa
+        ---------------------------
+        Seleccione una funcion: """))
+        if opc == 1:
+            ejercicio1()
+        elif opc == 2:
+            ejercicio2()
+        elif opc == 3:
+            ejercicio3()
+        elif opc == 4:
+            ejercicio4()
+        elif opc == 0:
+            print("Saliendo del programa...")
+            seguir = False
+menu()
