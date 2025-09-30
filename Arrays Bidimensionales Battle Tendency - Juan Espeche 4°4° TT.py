a462bcefd500617e6f06f6d376a550567f1df7ae
juanespeche_et32 <juan.especheet32@gmail.com>
2025-09-30 09:12:01 -0300

    Update Arrays Bidimensionales Battle Tendency - Juan Espeche 4°4° TT.py

--- Arrays Bidimensionales Battle Tendency - Juan Espeche 4°4° TT.py
+++ Arrays Bidimensionales Battle Tendency - Juan Espeche 4°4° TT.py
@@ -1,36 +1,20 @@
 def ejercicio1():
-   suma_fila1 = 0
-   suma_fila2 = 0
-   suma_fila3 = 0
-   suma_fila4 = 0
-   suma_colum1 = 0
-   suma_colum2 = 0
-   suma_colum3 = 0
-   suma_colum4 = 0
    matriz = [[1, 2, 3, 4],
              [5, 6, 7, 8],
              [9, 10, 11, 12],
              [13, 14, 15, 16]]
-   for x in matriz[0]:
-       suma_fila1 += x
-   for y in matriz[1]:
-       suma_fila2 += y
-   for p in matriz[2]:
-       suma_fila3 += p
-   for h in matriz[3]:
-       suma_fila4 += h
-   suma_colum1 = suma_colum1 + matriz[0][0] + matriz[1][0] + matriz[2][0] + matriz[3][0]
-   suma_colum2 = suma_colum2 + matriz[0][1] + matriz[1][1] + matriz[2][1] + matriz[3][1]
-   suma_colum3 = suma_colum3 + matriz[0][2] + matriz[1][2] + matriz[2][2] + matriz[3][2]
-   suma_colum4 = suma_colum4 + matriz[0][3] + matriz[1][3] + matriz[2][3] + matriz[3][3]
-   print(f"La suma de la primera fila es de: {suma_fila1}")
-   print(f"La suma de la segunda fila es de: {suma_fila2}")
-   print(f"La suma de la tercera fila es de: {suma_fila3}")
-   print(f"La suma de la cuarta fila es de: {suma_fila4}")
-   print(f"La suma de la primera columna es: {suma_colum1}")
-   print(f"La suma de la segunda columna es: {suma_colum2}")
-   print(f"La suma de la tercera columna es: {suma_colum3}")
-   print(f"La suma de la cuarta columna es: {suma_colum4}")
+   for x in matriz1:
+       filas_suma = 0
+       print(x)
+       for num in x:
+           filas_suma += num
+       print(f"La suma de esta fila es: {filas_suma}")
+   for x in range(len(matriz1)):
+       columnas_suma = 0
+       for y in range(len(matriz1[x])):
+           columnas_suma += matriz1[y][x]
+           print(matriz1[y][x])
+       print(f"La suma de esta Columna es: {columnas_suma}")
 
 def ejercicio2():
    traspuesta = [[],
@@ -41,11 +25,14 @@ def ejercicio2():
              [5, 6, 7, 8],
              [9, 10, 11, 12],
              [13, 14, 15, 16]]
+   for fila in matriz:
+       print("Matriz: ", fila)
    for x in range(len(matriz)):
        for y in range(len(matriz[x])):
            traspuesta[x].append(matriz[y][x])
-   for traspuestafinal in traspuesta:
-       print(traspuestafinal)
+       for traspuesta_final in traspuesta:
+           print(matriz_traspuesta_final)
+   
 
 def ejercicio3():
    contar_num = 0
@@ -53,21 +40,39 @@ def ejercicio3():
              [8, 5, 9, 2],
              [4, 5, 6, 7]]
    num_ingresado = int(input("Ingrese un número para ver cuantas veces se repite en la matriz: "))
-   for x in range(len(matriz)):
-       if num_ingresado in matriz[x]:
-           contar_num += matriz[x].count(num_ingresado)
+    for fila in matriz:
+        print(fila)
+        for numero in fila:
+            if numero == num_ingresado:
+                contador += 1
    print(f"El número ingresado aparece la cantidad de veces de: {contar_num}")
 
 def ejercicio4():
-    matriz = [[1, 2, 3, 4],
-              [5, 6, 7, 8],
-              [9, 10, 11, 12],
-              [13, 14, 15, 16]]
     promedio = 0
+    matriz = [[1, 5, 3, 5],
+                [8, 5, 9, 2],
+                [4, 5, 6, 7],
+                [2, 9, 1, 4]]
+    matriz_auxiliar = []
+    for fila in matriz:
+        print(fila)
+        for elemento in fila:
+            promedio += elemento
+    promedio = promedio/16
+    for fila in matriz:
+        lista_auxiliar = []
+        for elemento in fila:
+            if elemento < promedio:
+                lista_auxiliar.append(promedio)
+            else:
+                lista_auxiliar.append(elemento)
+        matriz_auxiliar.append((lista_auxiliar))
+    for x in matriz_auxiliar:
+        print(x)
 
 def menu():
     quiere_seguir = True
-    while quiere_seguir == True:
+    while quiere_seguir:
         opc = int(input("""
         ------------------------------------
         1.Suma de filas y columnas
@@ -88,4 +93,5 @@ def menu():
             ejercicio3()
         elif opc == 4:
             ejercicio4()
+
 menu()
