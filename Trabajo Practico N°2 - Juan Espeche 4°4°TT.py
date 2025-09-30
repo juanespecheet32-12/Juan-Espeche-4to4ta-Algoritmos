f4d283b2c56ab06604aae2155c6f3cec94acb68c
juanespeche_et32 <juan.especheet32@gmail.com>
2025-09-30 08:37:26 -0300

    Update Trabajo Practico N°2 - Juan Espeche 4°4°TT.py

--- Trabajo Practico N°2 - Juan Espeche 4°4°TT.py
+++ Trabajo Practico N°2 - Juan Espeche 4°4°TT.py
@@ -1,24 +1,29 @@
 import random
 def ahorcado():
     intentos = 7
-    palabras = ["pepino guau", "algoritmos", "computadora", "palabra", "loma de burro", "cartas"]
-    guion = "_"
-    palabra = ""
-    palabra_seleccionada = palabras[random.randint(0, 5)]
-    for x in range(len(palabra_seleccionada)):
-        palabra = guion * x
+    palabras = ["pepino guau", "algoritmos", "computadora", "palabra", "loma de burro"]
+    palabra_=[]
+    palabra_elegida = random.choice(palabras)
+    for x in range(len(palabra_elegida)):
+        palabra_.append("_")
     print("¡Bienvenido al ahorcado!")
     print("Se ha seleccionado una palabra al azar.")
-    while intentos != 0 or palabra == palabra_seleccionada:
-        opc = str(input(f"""
-La palabra esta formada: {palabra}
-Tienes {intentos} intentos.
-Ingrese una letra para saber si esta en la palabra: """))
-        if opc in palabra_seleccionada:
-            palabra = palabra
-            print("La letra se encontro en la palabra.")
-        else:
-            intentos = intentos - 1
-            print(f"""No se encontro la letra en la palabra seleccionada. 
-            Tienes {intentos} intentos.""")
+    while intentos>0:
+        print(f"{palabra_} intentos restantes: {intentos}")
+        letra_elegida = input("Ingrese cualquier letra para saber si esta en la palabra:")
+        adivino = False
+        for indice_letra in range (len(palabra_elegida)):
+            for letra2 in letra_elegida:
+                if letra2 == palabra_elegida[indice_letra]:
+                    palabra_[indice_letra] = letra2
+                    adivino = True
+        if not adivino:
+        intentos -= 1
+        if not "_" in palabra_:
+        print("Adivinaste toda la palabra sos muy bueno.")
+        break
+    if intentos<=0:
+        print("Se te acabaron los intentos y no adivinaste toda la palabra. Perdiste.")
+    
+
 ahorcado()
