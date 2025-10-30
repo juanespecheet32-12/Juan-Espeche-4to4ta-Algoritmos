e6a405b37841eb338fdb909fb3e70302584639af
juanespeche_et32 <juan.especheet32@gmail.com>
2025-10-30 13:25:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+num_fila = 10
+num_colum = 0
+matriz = [[1, 5, 3],
+          [4, 2, 6],
+          [9, 8, 7]]
+for x in range(0, len(matriz)):
+    for y in range(0, len(matriz)):
+
+
+
+print(num_fila)
+print(num_colum)
