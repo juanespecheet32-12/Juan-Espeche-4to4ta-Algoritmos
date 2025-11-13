26cd8dc004db0eb627659e5551c1c0ca763e02b4
juanespeche_et32 <juan.especheet32@gmail.com>
2025-11-13 13:22:59 -0300

    Add files via upload

---
+++ Trabajo Practico Unidad 2 Matrices - Juan Espeche 4°4° TT.py
@@ -0,0 +1,15 @@
+num_fila = 10
+num_colum = 0
+punto_de_silla = 0
+matriz = [[1, 5, 3],
+          [4, 2, 6],
+          [9, 8, 7]]
+for x in range(0, len(matriz)):
+    for y in range(0, len(matriz)):
+        valor1 = matriz[x][0]
+        valor2 = matriz[x][1]
+        valor3 = matriz[x][2]
+        if valor > matriz[x][y]:
+
+print(num_fila)
+print(num_colum)
